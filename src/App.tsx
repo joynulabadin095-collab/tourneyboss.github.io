010aaa1e9eaf0be23bd508eae7be7e5da5b9aa7c
joynulabadin095-collab <joynulabadin095@gmail.com>
2026-09-12 18:18:12 +0600

    Add RequireAuth for Profile route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import ProtectedRoute from './components/ProtectedRoute'
+import RequireAuth from './components/RequireAuth'
 import Home from './pages/Home'
 import TournamentList from './pages/TournamentList'
 import TournamentDetails from './pages/TournamentDetails'
@@ -8,6 +9,7 @@ import Login from './pages/Login'
 import PlayerDashboard from './pages/PlayerDashboard'
 import OrganizerDashboard from './pages/OrganizerDashboard'
 import AdminPanel from './pages/AdminPanel'
+import Profile from './pages/Profile'
 
 export default function App() {
   return (
@@ -18,6 +20,7 @@ export default function App() {
         <Route path="/tournaments" element={<TournamentList />} />
         <Route path="/tournaments/:id" element={<TournamentDetails />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<RequireAuth><Profile /></RequireAuth>} />
         <Route path="/player" element={
           <ProtectedRoute role="member"><PlayerDashboard /></ProtectedRoute>
         } />
