afad08ab7acb5b7c9fd0981cce2d19f7461b6e02
Joynul Abadin <joynulabadin095@gmail.com>
2026-09-22 15:39:26 +0600

    fix: remove GitHub Pages basename from router

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tourneyboss.github.io">
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
